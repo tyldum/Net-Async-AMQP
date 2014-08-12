3698bf94c459321d5467c90691d955da25203b63
Tom Molesworth <tom@perlsite.co.uk>
2014-08-12 05:05:16 +0000

    Simple load test script
    git-svn-id: http://entitymodel.com/svn/trunk/Net-Async-AMQP@2386 c5bf8c3b-bf20-4ed3-b413-d5547a49f493

---
+++ examples/loadtest.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use Net::Async::AMQP;
+use IO::Async::Loop;
+use IO::Async::Loop::Epoll;
+use IO::Async::Timer::Periodic;
+use Future::Utils qw(fmap0);
+use feature qw(say);
+
+use Getopt::Long;
+
+my %args;
+GetOptions(
+	"host|h=s" => \$args{host},
+	"user|u=s" => \$args{user},
+	"pass|p=s" => \$args{pass},
+	"port|P=i" => \$args{port},
+	"vhost|v=s" => \$args{vhost},
+) or die("Error in command line arguments\n");
+
+my $loop = IO::Async::Loop->new;
+
+say "start";
+my $total = 0; my $active = 0;
+$loop->add(IO::Async::Timer::Periodic->new(
+	interval => 2,
+	reschedule => 'skip',
+	on_tick => sub {
+		printf "%d total, %d active\n", $total, $active;
+	}
+)->start);
+my $true = (Net::AMQP->VERSION >= 0.06) ? Net::AMQP::Value->true : 1;
+my %mq;
+(fmap0 {
+	++$active;
+    my $mq = Net::Async::AMQP->new(
+        loop               => $loop,
+        heartbeat_interval => 0,
+    );
+    $mq{$mq} = $mq->connect(
+		%args,
+        client_properties => {
+            capabilities => {
+                'consumer_cancel_notify' => $true,
+                'connection.blocked'     => $true,
+            },
+        },
+	)->on_ready(sub { --$active; ++$total })
+} concurrent => 64, generate => sub { 1 })->get;
+
