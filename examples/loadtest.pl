217917ea75566e6d4f5f0b541a8f889cdaa6b3dd
Tom Molesworth <tom@perlsite.co.uk>
2014-08-12 12:42:05 +0000

    Move into a fork, add more config
    git-svn-id: http://entitymodel.com/svn/trunk/Net-Async-AMQP@2393 c5bf8c3b-bf20-4ed3-b413-d5547a49f493

--- examples/loadtest.pl
+++ examples/loadtest.pl
@@ -12,12 +12,16 @@ use feature qw(say);
 use Getopt::Long;
 
 my %args;
+$args{localhost} = [];
 GetOptions(
-	"host|h=s" => \$args{host},
-	"user|u=s" => \$args{user},
-	"pass=s" => \$args{pass},
-	"port=i" => \$args{port},
-	"vhost|v=s" => \$args{vhost},
+	"host|h=s"    => \$args{host},
+	"user|u=s"    => \$args{user},
+	"pass=s"      => \$args{pass},
+	"port=i"      => \$args{port},
+	"vhost|v=s"   => \$args{vhost},
+	"parallel=i"  => \$args{parallel},
+	"forks=i"     => \$args{forks},
+	"localhost=s" => $args{localhost},
 ) or die("Error in command line arguments\n");
 
 my $loop = IO::Async::Loop->new;
@@ -37,26 +41,72 @@ $loop->add(IO::Async::Timer::Periodic->new(
 )->start);
 my $true = (Net::AMQP->VERSION >= 0.06) ? Net::AMQP::Value->true : 1;
 my %mq;
-(fmap0 {
-	++$stats{active};
-    my $mq = Net::Async::AMQP->new(
-        loop               => $loop,
-        heartbeat_interval => 0,
-    );
-	my $k = "$mq";
-	$mq{$k} = Future->wait_any(
-		$mq->connect(
-			%args,
-			client_properties => {
-				capabilities => {
-					'consumer_cancel_notify' => $true,
-					'connection.blocked'     => $true,
-				},
-			},
-		)->on_fail(sub { ++$stats{failed} }),
-		$loop->timeout_future(after => 15)
-		 ->on_fail(sub { ++$stats{timeout} })
-	)->on_ready(sub { --$stats{active}; ++$stats{total} })
-	 ->on_fail(sub { delete $mq{$k} })
-} concurrent => 128, generate => sub { 1 })->get;
+my @hosts = @{ delete($args{localhost}) || [qw(localhost)] };
+my $parallel = delete $args{parallel} || 128;
+my $forks = delete $args{forks} || 1;
+
+for my $idx (1..$forks) {
+	my $loop_class = ref($loop);
+	$loop->add(IO::Async::Process->new(
+		stdin => { from => '' },
+		stdout => {
+			on_read => sub {
+				my ($stream, $buffref, $eof) = @_;
+				while($$buffref =~ s/^(.*)\n//) {
+					my ($k, $v) = split /:/, $1;
+					$stats{$k} += $v;
+				}
+				warn "Closed input" if $eof;
+				return 0
+			}
+		},
+		on_finish => sub { warn "Finished: @_\n" },
+		code => sub {
+		eval {
+			my $loop = $loop_class->new;
+			$loop->resolver->configure(
+				min_workers => 1,
+				max_workers => 1,
+			);
+			$loop->add(my $stdout = IO::Async::Stream->new_for_stdout);
+			(fmap0 {
+				$stdout->write("active:1\n");
+				my $mq = Net::Async::AMQP->new(
+					loop               => $loop,
+					heartbeat_interval => 0,
+				);
+				my $k = "$mq";
+				push @hosts, my $host = shift @hosts;
+				$mq{$k} = Future->wait_any(
+					$mq->connect(
+						%args,
+						local_host => $host,
+						client_properties => {
+							capabilities => {
+								'consumer_cancel_notify' => $true,
+								'connection.blocked'     => $true,
+							},
+						},
+					)->on_fail(sub {
+						$stdout->write("failed:1\n");
+						warn "Failure: @_\n"
+					})->on_done(sub {
+						$stdout->write("success:1\n");
+					}),
+					$loop->timeout_future(after => 30)
+					 ->on_fail(sub {
+						$stdout->write("timeout:1\n");
+					})
+				)->on_ready(sub {
+					$stdout->write("active:-1\n");
+					$stdout->write("total:1\n");
+				})
+				 ->on_fail(sub { delete $mq{$k} })
+				 ->else(sub { Future->wrap })
+			} concurrent => $parallel, generate => sub { 1 })->get;
+			1 } or warn "Failed: $@\n";
+		}
+	));
+}
+$loop->run;
 
