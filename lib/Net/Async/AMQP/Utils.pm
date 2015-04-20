c50c84acd2e1bbf4d1b5a84f83261a9b23ba60c2
Tom Molesworth <tom@perlsite.co.uk>
2015-04-20 00:43:04 +0000

    Helper functions for converting error codes to message/description
    git-svn-id: http://entitymodel.com/svn/trunk/Net-Async-AMQP@3234 c5bf8c3b-bf20-4ed3-b413-d5547a49f493

--- lib/Net/Async/AMQP/Utils.pm
+++ lib/Net/Async/AMQP/Utils.pm
@@ -75,6 +75,66 @@ sub amqp_frame_type {
 }
 }
 
+{
+my %amqp_codes = (
+	200 => { message => 'reply­success', description => 'Indicates that the method completed successfully. This reply code is reserved for future use ­ the current protocol design does not use positive confirmation and reply codes are sent only in case of an error.' },
+	311 => { message => 'content­too­large', type => 'channel', description => 'The client attempted to transfer content larger than the server could accept at the present time. The client may retry at a later time.' },
+	313 => { message => 'no­consumers', type => 'channel', description => 'When the exchange cannot deliver to a consumer when the immediate flag is set. As a result of pending data on the queue or the absence of any consumers of the queue.' },
+	320 => { message => 'connection­forced', type => 'connection', description => 'An operator intervened to close the connection for some reason. The client may retry at some later date.' },
+	402 => { message => 'invalid­path', type => 'connection', description => 'The client tried to work with an unknown virtual host.' },
+	403 => { message => 'access­refused', type => 'channel', description => 'The client attempted to work with a server entity to which it has no access due to security settings.' },
+	404 => { message => 'not­found', type => 'channel', description => 'The client attempted to work with a server entity that does not exist.' },
+	405 => { message => 'resource­locked', type => 'channel', description => 'The client attempted to work with a server entity to which it has no access because another client is working with it.' },
+	406 => { message => 'precondition­failed', type => 'channel', description => 'The client requested a method that was not allowed because some precondition failed.' },
+	501 => { message => 'frame­error', type => 'connection', description => 'The sender sent a malformed frame that the recipient could not decode.  This strongly implies a programming error in the sending peer.' },
+	502 => { message => 'syntax­error', type => 'connection', description => 'The sender sent a frame that contained illegal values for one or more fields.  This strongly implies a programming error in the sending peer.' },
+	503 => { message => 'command­invalid', type => 'connection', description => 'The client sent an invalid sequence of frames, attempting to perform an operation that was considered invalid by the server. This usually implies a programming error in the client.' },
+	504 => { message => 'channel­error', type => 'connection', description => 'The client attempted to work with a channel that had not been correctly opened. This most likely indicates a fault in the client layer.' },
+	505 => { message => 'unexpected­frame', type => 'connection', description => 'The peer sent a frame that was not expected, usually in the context of a content header and body. This strongly indicates a fault in the peer\'s content processing.' },
+	506 => { message => 'resource­error', type => 'connection', description => 'The server could not complete the method because it lacked sufficient resources. This may be due to the client creating too many of some type of entity.' },
+	530 => { message => 'not­allowed', type => 'connection', description => 'The client tried to work with some entity in a manner that is prohibited by the server, due to security settings or by some other criteria.' },
+	540 => { message => 'not­implemented', type => 'connection', description => 'The client tried to use functionality that is not implemented in the server.' },
+	541 => { message => 'internal­error', type => 'connection', description => 'The server could not complete the method because of an internal error. The server may require intervention by an operator in order to resume normal operations.' },
+);
+
+=head2 message_for_code
+
+Returns the name (short message) corresponding to the given status code.
+
+Example:
+
+ message_for_code(540) => 'not implemented'
+
+=cut
+
+sub message_for_code {
+	my ($code) = @_;
+	$amqp_codes{$code}{message}
+}
+
+=head2 description_for_code
+
+Returns the description (long message) corresponding to the given status code.
+
+=cut
+
+sub description_for_code {
+	my ($code) = @_;
+	$amqp_codes{$code}{description}
+}
+
+=head2 type_for_code
+
+Returns the type for the given status code - typically "channel" or "connection".
+
+=cut
+
+sub type_for_code {
+	my ($code) = @_;
+	$amqp_codes{$code}{type}
+}
+}
+
 1;
 
 __END__
